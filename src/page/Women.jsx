0b253748d4e44867c13c00e0551e1c504f5b542c
ozi <ozisadiqkasai@gmail.com>
2023-11-13 12:09:15 +0100

    modified files

--- src/page/Women.jsx
+++ src/page/Women.jsx
@@ -14,24 +14,21 @@ const Women = () => {
   const item = filteredWomenItem.map((item) => (
     <section
       key={item.id}
-      className="flex w-[250px] h-[400px] justify-center items-center font-lato flex-col ax-tiny:w-full max-tiny:mt-16"
+      className="tiny:w-[300px] font-lato"
     >
-      <div className="max-tiny:w-auto">
-        <img
-          src={item.image}
-          alt={item.name}
-          className="w-48 h-56 max-md:w-24 m-auto"
-        />
-        <h2 className="font-bold text-sm leading-5">{item.title}</h2>
-        <p className="py-1">${item.price}</p>
+      <img src={item.image} alt={item.name} className="w-36 m-auto" />
+
+      <h2 className="font-semibold leading-5 text-medium w-64">{item.title}</h2>
+      <p className="">${item.price}</p>
+      <div className="flex flex-col">
         <Link
           to={`/details/${item.id}`}
-          className="font-light text-sm hover:text-orange-text text-slate-400 "
+          className="hover:text-orange-700 cursor-pointer text-sm"
         >
           more details
         </Link>
         <button
-          className="bg-search py-1 px-3 text-slate-100 w-full"
+          className="bg-orange-300 text-slate-200 hover:text-orange-300 hover:bg-header py-1 w-44"
           onClick={() => addToCart(item, item.id)}
         >
           Add to Cart
@@ -40,7 +37,7 @@ const Women = () => {
     </section>
   ))
   return (
-    <main className="flex justify-around items-center flex-wrap py-24 gap-4 mt-5 max-tiny:p-4 ">
+    <main className="mt-24 flex flex-wrap gap-8 justify-around items-center pb-12 pt-2">
       {item}
     </main>
   )
